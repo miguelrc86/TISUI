d4341640d092a9e0aa1e8921584a09bab96b87c4
Miguel Rojas Cortes <me@miguelcortes.co>
2021-11-03 19:37:08 -0500

    Initial Commit

---
+++ TISUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TISUI
+//
+//  Created by Miguel Rojas Cortes on 11/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TISUI/TISUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TISUIApp.swift
+//  TISUI
+//
+//  Created by Miguel Rojas Cortes on 11/3/21.
+//
+
+import SwiftUI
+
+@main
+struct TISUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
