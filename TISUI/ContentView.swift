9a729ffcc61c7366d1650a6f37f8030bfd31460e
Miguel Rojas Cortes <me@miguelcortes.co>
2021-11-10 20:40:02 -0500

    Chapter One

--- TISUI/ContentView.swift
+++ TISUI/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State var counter = 0
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Button(action: { counter += 1 }) {
+                Text("Tap me")
+                    .padding()
+                    .background(Color(.tertiarySystemFill))
+                    .cornerRadius(5)
+            }
+
+            if counter > 0 {
+                Text("You have tapped \(counter) times")
+            } else {
+                Text("You have not yet tapped")
+            }
+        }.debug()
+            .frame(width: 200, height: 200)
+            .border(Color.black)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension View {
+
+    func debug() -> Self {
+        print(Mirror(reflecting: self).subjectType)
+
+        return self
     }
 }
